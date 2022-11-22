15cdc24844e6e460605816d4bfa72ef7abb9b597
Christian Buffa <christian.b2402@gmail.com>
2022-11-22 15:43:32 +0100

    add input system and tank controls

--- SpaceRubbleCleanerUnityProject/Assets/Scripts/PlayerMovement.cs
+++ SpaceRubbleCleanerUnityProject/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,32 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private InputAction;
-    
-    
+    private float playerSpeed;
+    [SerializeField]
+    private float playerRotation;
 
+    private CharacterController controller;
+
+    private Vector2 playerInput;
+
+    private void Start()
+    {
+        controller =  GetComponent<CharacterController>();        
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        controller.Move(transform.forward * playerInput.y * playerSpeed * Time.deltaTime);
+        transform.Rotate(transform.up, playerRotation * playerInput.x * Time.deltaTime);
+    }
+
+    private void OnMovement(InputValue value)
+    {
+        playerInput = value.Get<Vector2>();
+    }
 }
