bcb0f542b7544d5a0853624924f0689ba2b3a33e
Christian Buffa <christian.b2402@gmail.com>
2022-12-11 22:26:36 +0100

    added fuel system and fuel ui

--- SpaceRubbleCleanerUnityProject/Assets/Scripts/PlayerMovement.cs
+++ SpaceRubbleCleanerUnityProject/Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] 
     private float maxFuel;
+    [SerializeField] 
+    private float fuelBurnRate;
     [SerializeField]
     private float playerSpeed;
     [SerializeField]
     private float playerRotation;
+    [SerializeField] 
+    private Slider fuelSlider;
 
     private Rigidbody rb;
     private float currentFuel;
@@ -28,12 +33,22 @@ public class PlayerMovement : MonoBehaviour
 
     private void Movement()
     {
+        fuelSlider.value = currentFuel / maxFuel;
+        
         rb.AddForce(transform.forward * playerInput.y * playerSpeed * Time.deltaTime, ForceMode.VelocityChange);
         transform.Rotate(transform.up, playerRotation * playerInput.x * Time.deltaTime);
     }
 
     private void OnMovement(InputValue value)
     {
-        playerInput = value.Get<Vector2>();
+        if (currentFuel > 0)
+        {
+            currentFuel -= fuelBurnRate * Time.deltaTime;
+            playerInput = value.Get<Vector2>();
+        }
+        else
+        {
+            playerInput = Vector2.zero;
+        }
     }
 }
